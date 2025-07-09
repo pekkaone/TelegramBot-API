0335c92b7701055687e724cc64ff74938eb59351
danylokostiuk <danylokostiukhuk@gmail.com>
2025-07-09 18:43:51 +0200

    pizdefc

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI
 from bot import run_bot
 import asyncio
 
-app = FastAPI
+app = FastAPI()
 
 @app.get('/')
 async def root():
     return {"bot": "running"}
 
-@app.on_event("startap")
+@app.on_event("startup")
 async def on_startap():
     asyncio.create_task(run_bot())
