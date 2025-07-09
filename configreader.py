0ce5ec2215f027475b361e10db53d96bd9401a09
danylokostiuk <danylokostiukhuk@gmail.com>
2025-07-09 18:33:03 +0200

    ADDED FASTAPI

--- configreader.py
+++ configreader.py
@@ -1,8 +1,8 @@
-from pydantic import SecretStr
+from pydantic import SecretStr, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    bot_token: SecretStr
+    bot_token: SecretStr = Field(..., env="BOT_TOKEN")
 
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
--- main.py
+++ main.py
@@ -1,43 +1,13 @@
+from fastapi import FastAPI
+from bot import run_bot
 import asyncio
-import logging
-from datetime import datetime
-from aiogram import Bot, Dispatcher, types, F
-from aiogram.types import Message
-from aiogram.filters.command import Command
-from aiogram.enums.dice_emoji import DiceEmoji
-from configreader import config
 
-logging.basicConfig(level=logging.INFO)
-bot = Bot(token=config.bot_token.get_secret_value())
-dp = Dispatcher()
+app = FastAPI
 
-@dp.message(Command('start'))
-async def start_command(message: types.Message):
-    await message.answer("This is a testing bot, gladly working\nMade by @Pekka_One")
+@app.get('/')
+async def root():
+    return {"bot": "running"}
 
-@dp.message(F.text.lower() == "bitch")
-async def bad_word(message: Message):
-    user_id = message.from_user.id
-    def_username = 1438774321
-    await bot.send_message(chat_id=def_username, text=f"You shoud beat {user_id} ass))")
-    await message.answer("You are gonna regret")
-
-@dp.message(Command('myid'))
-async def user_id(message: Message):
-    my_id = message.from_user.id
-    my_else = message.from_user.username
-    my_else2 = message.from_user.is_premium
-    my_else3 = message.from_user.full_name
-    await message.reply(f"your id is {my_id}\nUsername is @{my_else}\nis Premium: {my_else2}\nFull name: {my_else3}")
-
-
-@dp.message()
-async def not_recognised(message: Message):
-    await message.answer("Not recognised")
-
-
-async def main():
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+@app.on_event("startap")
+async def on_startap():
+    asyncio.create_task(run_bot())
